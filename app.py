3c7ee1efe16e82efb3d508b137130b5cfcbd7960
Muhammad Shujaat <shujat.jamshed@yahoo.com>
2026-09-20 03:54:47 +0500

    configure for production deployment

--- app.py
+++ app.py
@@ -837,4 +837,6 @@ def get_branch_mapping():
 
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5002, debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
